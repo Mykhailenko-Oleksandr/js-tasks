ba6739d6d835c836382950f0d4cb58573a0ba822
Sapper <saniamykhailenko@gmail.com>
2025-06-23 20:49:39 +0300

    add 1 day modules 8

---
+++ js/lessions/les-15.js
@@ -0,0 +1,27 @@
+const parent = document.querySelector("#parent");
+const child = document.querySelector("#child");
+const descendant = document.querySelector("#descendant");
+
+parent.addEventListener("click", () => {
+    alert(
+        "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+    );
+});
+
+child.addEventListener("click", () => {
+    alert(
+        "Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+    );
+});
+
+descendant.addEventListener("click", (event) => {
+    event.stopPropagation();
+    alert("Descendant click handler");
+});
+
+
+const box = document.querySelector("div.box");
+
+box.addEventListener("click", function (event) {
+    console.log(event.target.nodeName);
+});
